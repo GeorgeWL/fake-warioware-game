268a2e0cfeeb4e9f4d89d3ba1b692e0a089c9536
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-10-03 01:05:28 +0100

    lifeicon use svgs

--- src/components/lifeTracking/LifeIcon.js
+++ src/components/lifeTracking/LifeIcon.js
@@ -1,22 +1,45 @@
 import styles from './lifeIcon.module.scss';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { ReactComponent as Heart } from '../../assets/heart.svg';
+import { ReactComponent as Star } from '../../assets/star.svg';
 
-const LifeIcon = ({ isHardCore, size = 'medium', backgroundColor }) => (
-  <div
-    className={classNames(
-      styles.heart,
-      isHardCore && styles['heart--hardcore'],
-      styles[`heart--${size}`]
-    )}
-    style={{ backgroundColor }}
-  />
-);
+const LifeIcon = ({ isHardCore, size = 'medium', backgroundColor='red', hardcoreBorderColor='gold', iconName='heart' }) => {
+  const commonProps = {
+    className: classNames(
+      styles.icon,
+      isHardCore && styles['icon--hardcore'],
+      styles[`icon--${size}`]
+    ),
+    style: { 
+      fill: backgroundColor,
+      filter: isHardCore&& `drop-shadow(1px 2px 5px ${hardcoreBorderColor})` 
+    },
+  };
+  switch (iconName) {
+  case 'star':
+    return( 
+      <Star
+        {...commonProps}
+      />
+    );
+  case 'heart':
+  default:
+    return(
+      <Heart
+        {...commonProps}
+      />
+    );
+  
+  }
+};
 
 LifeIcon.propTypes = {
   isHardCore: PropTypes.bool,
   backgroundColor: PropTypes.string,
+  hardcoreBorderColor: PropTypes.string,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
+  iconName: PropTypes.oneOf(['heart', 'star'])
 };
 
 export default LifeIcon;
