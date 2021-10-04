0779fe4e6c20fbda1cdafe1353e68582b311eff0
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-10-04 21:45:23 +0100

    move button and link to generics

---
+++ src/components/generic/Button.js
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './button.module.scss';
+
+/**
+ * Primary UI component for user interaction
+ */
+const Button = ({  isPrimary, backgroundColor, textColor, size, label, disabled, ...props }) => {
+
+  const backgroundColorStyle =
+    typeof backgroundColor === 'string' && backgroundColor
+      ? { backgroundColor }
+      : {};
+  const textColorStyle = typeof textColor ==='string' && textColor ? { color: textColor } : {};
+  const styleObject = { ...backgroundColorStyle, ...textColorStyle };
+
+  return (
+    <button
+      type='button'
+      className={classNames(styles.button, styles[`button--${size}`], styles[`button--${isPrimary ? 'primary' : 'secondary' }`] )}
+      disabled={disabled}
+      style={styleObject}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  isPrimary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * What background color to use
+   */
+  textColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf([ 'small', 'medium', 'large' ]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool
+};
+
+Button.defaultProps = {
+  backgroundColor: 'lightblue',
+  textColor: 'white',
+  primary: false,
+  size: 'medium',
+  onClick: undefined,
+};
+
+export default Button;
--- src/components/generic/Link.js
+++ src/components/generic/Link.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './link.module.scss';
 
-const Link = ({ href, newTab = true, type = 'primary',disabled, children }) => {
+const Link = ({ href, newTab, type, disabled, children, ...props }) => {
   const newTabProps = newTab
     ? { target: '_blank', rel: 'noreferrer noopener' }
     : {};
@@ -10,8 +10,9 @@ const Link = ({ href, newTab = true, type = 'primary',disabled, children }) => {
   return (
     <a
       href={href}
-      className={classNames(styles.link, type && styles[`link--${type}`],disabled&&styles['link--disabled'])}
+      className={classNames(styles.link, type && styles[`link--${type}`], disabled&&styles['link--disabled'])}
       {...newTabProps}
+      {...props}
     >
       {children}
     </a>
@@ -19,11 +20,17 @@ const Link = ({ href, newTab = true, type = 'primary',disabled, children }) => {
 };
 
 Link.propTypes = {
-  disabled:PropTypes.bool,
+  disabled: PropTypes.bool,
   href: PropTypes.string.isRequired,
   newTab: PropTypes.bool,
   children: PropTypes.node.isRequired,
-  type: PropTypes.oneOf(['primary', 'secondary']),
+  type: PropTypes.oneOf([ 'primary', 'secondary' ]),
+};
+
+Link.defaultProps = {
+  newTab: true,
+  type: 'primary',
+  disabled: false
 };
 
 export default Link;
