faead041b6058a7be9b3c9b9e06b67b904379364
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-10-10 23:06:06 +0100

    label basic

---
+++ src/components/generic/Label.js
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './label.module.scss';
+
+const Label = ({ children, primary })=>(
+  <label
+    className={classNames(styles.label, primary&&styles['label--primary'])}
+  >
+    {children}
+  </label>
+);
+
+Label.propTypes={
+  children: PropTypes.node,
+  primary: PropTypes.bool
+};
+
+export default Label;
