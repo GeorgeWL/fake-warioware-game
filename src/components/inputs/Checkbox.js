7ff3106a20ee441afd8b57bafdc04d888ec0993a
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-10-10 23:06:23 +0100

    checkbox with intenral state managment

--- src/components/inputs/Checkbox.js
+++ src/components/inputs/Checkbox.js
@@ -1,30 +1,63 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import uuid from '../../uuid';
 import styles from './checkbox.module.scss';
 
-const Checkbox = ({ onClick, checked, isPrimary }) => (
-  <div
-    className={classNames(
-      styles.checkbox,
-      isPrimary && styles['checkbox--primary']
-    )}
-  >
-    <input
-      type='checkbox'
-      onClick={onClick}
-      checked={checked}
-    />
-  </div>
-);
+
+const Checkbox = ({ onClick,  size, disabled, title, accentColor, iconColor, tickIcon }) => {
+  const id = uuid();
+  const [ checked, setChecked ]=useState(false);
+
+  const handleClick = ()=>{
+    onClick(checked);
+    setChecked(prevState=>!prevState);
+  };
+
+  return(
+    <div
+      className={classNames(
+        styles.checkbox,
+        disabled&&styles['checkbox--disabled'],
+        styles[`checkbox--${size}`]
+      )}
+    >
+      <label
+        htmlFor={id}
+        title={title}
+        style={{
+          backgroundColor: checked ? accentColor : 'white',
+          borderColor: accentColor,
+          color: checked ? iconColor : 'transparent'
+        }}
+      >
+        {checked&&tickIcon}
+      </label>
+      <input
+        disabled={disabled}
+        type="checkbox" id={id}
+        onClick={handleClick}
+        checked={checked}
+      />
+    </div>
+  );};
 
 Checkbox.propTypes = {
   onClick: PropTypes.func.isRequired,
-  checked: PropTypes.bool,
-  isPrimary: PropTypes.bool,
+  title: PropTypes.string,
+  size: PropTypes.oneOf([ 'small', 'medium', 'large' ]),
+  accentColor: PropTypes.string,
+  iconColor: PropTypes.string,
+  disabled: PropTypes.bool,
+  tickIcon: PropTypes.oneOf([ '✔', '🔪', '⚡', '❤', '🎁', '👍', '😁', '🎤' ])
 };
 
 Checkbox.defaultProps = {
-  isPrimary: true,
+  primary: true,
+  accentColor: '#00c0ff',
+  size: 'medium',
+  disabled: false,
+  tickIcon: '✔'
 };
 
 export default Checkbox;
