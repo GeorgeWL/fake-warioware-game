8c68ee83274c98ab9a483fddcca277385ae0bfea
langhag <george.langham@autodesk.com>
2021-10-14 14:54:27 +0100

    change mediaslice action to setters

--- src/app/mediaSlice.js
+++ src/app/mediaSlice.js
@@ -9,11 +9,11 @@ export const mediaSlice = createSlice({
   name: 'media',
   initialState,
   reducers: {
-    toggleVoice: (state) => (state.voiceEnabled = !state.voiceEnabled),
-    toggleCamera: (state) => (state.cameraEnabled = !state.cameraEnabled),
+    setVoiceEnabled: (state, action) => (state.voiceEnabled = action.payload),
+    setCameraEnabled: (state, action) => (state.cameraEnabled = action.payload),
   },
 });
-export const { toggleVoice, toggleCamera } = mediaSlice.actions;
+export const { setVoiceEnabled, setCameraEnabled } = mediaSlice.actions;
 export const selectCamera = (state) => state.media.cameraEnabled;
 export const selectVoice = (state) => state.media.voiceEnabled;
 export default mediaSlice.reducer;
