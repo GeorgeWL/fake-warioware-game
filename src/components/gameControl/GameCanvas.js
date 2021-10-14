60d83d1bb1e4a9e45f915a6a9e36260963566615
langhag <george.langham@autodesk.com>
2021-10-14 15:36:29 +0100

    automated formatting

--- src/components/gameControl/GameCanvas.js
+++ src/components/gameControl/GameCanvas.js
@@ -3,7 +3,13 @@ import styles from './gameCanvas.module.scss';
 
 // import * as PIXI from 'pixi.js'
 // TODO: Add Pixi
-const GameCanvas = ({ id, gameCompleted })=><canvas className={styles.container} onClick={gameCompleted} id={id}></canvas>;
+const GameCanvas = ({ id, gameCompleted }) => (
+  <canvas
+    className={styles.container}
+    onChange={gameCompleted}
+    id={id}
+  ></canvas>
+);
 
 GameCanvas.propTypes = {
   id: PropTypes.string.isRequired,
