d139bb2b563532b700dc8027c9739f84ca9fb9fd
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-11-28 16:37:17 +0000

    move iconnames to constant

---
+++ src/constants/IconNameTypes.js
@@ -0,0 +1,13 @@
+export const IconNameTypes = {
+  CHECK: '✔',
+  KNIFE: '🔪',
+  BOLT: '⚡',
+  HEART: '❤',
+  GIFT: '🎁',
+  THUMBS_UP: '👍',
+  THUMBS_DOWN: '👎',
+  HAPPY: '😁',
+  MICROPHONE: '🎤',
+  CAMERA: '📷',
+  VIDEO_CAM: '🎥'
+};
